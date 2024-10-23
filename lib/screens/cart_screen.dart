49693254a14e2fb69bbe2c6421ed2c042436677b
PapiPhilia <danielazarya123@gmail.com>
2024-10-23 23:30:36 +0700

    update

---
+++ lib/screens/cart_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CartScreen extends StatefulWidget {
+  const CartScreen({super.key});
+
+  @override
+  State<CartScreen> createState() => _CartScreen();
+}
---
+++ lib/screens/product_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ProductScreen extends StatefulWidget {
+  const ProductScreen({super.key});
+
+  @override
+  State<ProductScreen> createState() => _ProductScreen();
+}
+
